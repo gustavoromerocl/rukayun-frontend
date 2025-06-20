67161a1f038038fe37b24b0315180d52ad7636da
gustavoromerocl <gandresrp@gmail.com>
2025-06-19 22:41:49 -0400

    feat: add zustand

---
+++ src/lib/store.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface AppState {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+export const useAppStore = create<AppState>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+})); 
