5bb0e8176e7de1fd4e25f29ef08b230369650844
gustavoromerocl <gandresrp@gmail.com>
2025-06-26 08:54:31 -0400

    fix: update client id

--- src/lib/msalConfig.ts
+++ src/lib/msalConfig.ts
@@ -2,7 +2,7 @@ import type { Configuration } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: "5a85e056-9f5f-45c0-a9c5-02dd2a9f7655", // Tu Application (client) ID
+    clientId: "ce14b156-1e4a-4e31-bdc0-6b61de980f95", // Tu Application (client) ID
     authority: "https://accessmanagercloudnative1.b2clogin.com/accessmanagercloudnative1.onmicrosoft.com/B2C_1_singinsignup_rukayun",
     knownAuthorities: ["accessmanagercloudnative1.b2clogin.com"],
     redirectUri: "http://localhost:5173/dashboard", // O tu URL de producción
